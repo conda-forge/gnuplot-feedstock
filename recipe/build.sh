11182dffdaa23eb9ffd885acd799ee09e91b8f0f
Christian Roth <christian.roth@mpibpc.mpg.de>
2018-11-20 06:22:57 +0100

    add qt include paths recursively

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,22 @@ then
 	export LDFLAGS="$LDFLAGS -L $PREFIX/lib -liconv"
 fi
 
+
+# fix build error in macOS with system compilers:
+# - qtterminal/qt_term.cpp:51:10: fatal error: 'QtCore' file not found
+#- #include <QtCore>
+
+if [ "$(uname)" == "Darwin" ]
+then
+	export CXXFLAGS="$CXXFLAGS -I$PREFIX/include/qt"
+	export CXXFLAGS="$CXXFLAGS -I$PREFIX/include/qt/QtCore"
+	export CXXFLAGS="$CXXFLAGS -I$PREFIX/include/qt/QtGui"
+	export CXXFLAGS="$CXXFLAGS -I$PREFIX/include/qt/QtWidgets"
+	export CXXFLAGS="$CXXFLAGS -I$PREFIX/include/qt/QtPrintSupport"
+	export CXXFLAGS="$CXXFLAGS -I$PREFIX/include/qt/QtSvg"
+	export CXXFLAGS="$CXXFLAGS -I$PREFIX/include/qt/QtNetwork"
+fi
+
 ./configure \
 	--prefix=$PREFIX \
 	--without-x \
