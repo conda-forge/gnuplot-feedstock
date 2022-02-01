204f43027d712606742406413848fb7ec0c3b6bc
Tobias Fischer <info@tobiasfischer.info>
2022-02-01 14:55:02 +1000

    No qt on osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+set -x
+
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+if [[ $target_platform == "osx-arm64" ]]; then
+	QT="--without-qt"
+else
+	QT="--with-qt=qt5"
+fi
+
+export CC_FOR_BUILD=$CC_FOR_BUILD
 
 ./configure \
 	--prefix=$PREFIX \
 	--without-lua \
 	--without-latex \
 	--without-libcerf \
-	--with-qt=qt5 \
+	$QT \
 	--with-readline=$PREFIX \
 	--without-tutorial \
 	--disable-dependency-tracking
@@ -17,5 +27,7 @@ export GNUTERM=dumb
 # Fix iconv linkage
 sed -ie 's/\(^LIBS.*\)/\1 -liconv/g' src/Makefile
 make -j${CPU_COUNT} PREFIX=$PREFIX
-make check PREFIX=$PREFIX
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+ 	make check PREFIX=$PREFIX
+fi
 make install PREFIX=$PREFIX
