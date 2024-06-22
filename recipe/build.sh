5b3b109c56887c39cb0d00315924926fb4529507
Christian Roth <code@christianroth.dev>
2024-06-22 21:30:47 +0200

    uneducated-guess: also hack -liconv fix into native build

--- recipe/build.sh
+++ recipe/build.sh
@@ -24,20 +24,22 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     LDFLAGS_LD_FOR_BUILD=$(echo $LDFLAGS_LD | sed "s?$PREFIX?$BUILD_PREFIX?g")
     ../configure --prefix="${BUILD_PREFIX}" \
      	--without-lua \
-	--without-latex \
-	--without-libcerf \
-	--with-qt=no \
-	--without-readline \
-	--without-cairo \
-	--disable-dependency-tracking \
-	CC=$CC_FOR_BUILD \
-        CXX=$CXX_FOR_BUILD \
-        AR=${build_alias}-ar \
-        LD=${build_alias}-ld \
-        RANLIB=${build_alias}-ranlib \
-        LDFLAGS="$LDFLAGS_FOR_BUILD" \
-        LDFLAGS_LD="$LDFLAGS_LD_FOR_BUILD"
-
+      --without-latex \
+      --without-libcerf \
+      --with-qt=no \
+      --without-readline \
+      --without-cairo \
+      --disable-dependency-tracking \
+      CC=$CC_FOR_BUILD \
+      CXX=$CXX_FOR_BUILD \
+      AR=${build_alias}-ar \
+      LD=${build_alias}-ld \
+      RANLIB=${build_alias}-ranlib \
+      LDFLAGS="$LDFLAGS_FOR_BUILD" \
+      LDFLAGS_LD="$LDFLAGS_LD_FOR_BUILD"
+   
+    # Fix iconv linkage
+    sed -ie 's/\(^LIBS.*\)/\1 -liconv/g' ./src/Makefile
     make -j${CPU_COUNT} PREFIX=$BUILD_PREFIX
     make install PREFIX=$BUILD_PREFIX
     popd
