e5a3421c6b069a399c1ce8dd7659ab4f04fb5f5b
Christian Roth <code@christianroth.dev>
2024-06-22 20:49:25 +0200

    maint: clean up cdt packages

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,17 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
   # some tools are built and later executed in the build process
   # this requires a native build first
+  if [[ "${build_platform}" == "linux-64" ]]; then
+    # There are probably equivalent CDTs to install if your build platform
+    # is something else. However, it is most common in 2023 to use the x86_64
+    # hardware to cross compile for other architectures.
+    mamba install --yes \
+      --prefix ${BUILD_PREFIX} \
+      mesa-libgl-devel-${cdt_name}-x86_64  \
+      mesa-dri-drivers-${cdt_name}-x86_64 \
+      libselinux-${cdt_name}-x86_64  \
+      xorg-x11-proto-devel-${cdt_name}-x86_64
+  fi
   (
     mkdir -p build-native
     pushd build-native
